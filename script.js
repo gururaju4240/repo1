ad1ca6b839a65ad828011fd88c2c8275ed6f3363
gururaju <gururaju4240@gmail.com>
2024-10-24 18:37:05 +0530

    upload all 3 files  for mycart.com

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleLogin() {
+    alert('Login clicked');
+    // Add login functionality here
+}
+
+function toggleSignup() {
+    alert('Sign Up clicked');
+    // Add signup functionality here
+}
